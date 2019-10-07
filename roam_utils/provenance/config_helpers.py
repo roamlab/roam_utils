ac06cdcd67ea70582d41de951867f76dd8536b38
ehannigan <ejh2192@columbia.edu>
2019-10-07 14:52:08 -0400

    removed provenance.py reduced path generator, and moved items to config helpers

--- roam_utils/provenance/config_helpers.py
+++ roam_utils/provenance/config_helpers.py
@@ -1,4 +1,5 @@
 import ast
+import shutil
 from configparser import ConfigParser
 
 
@@ -62,3 +63,19 @@ def get_section_config_data(config_data, section_name):
     section_config_data = ConfigParser()
     recursive_copy_section_from_old_config_to_new_config(config_data, section_config_data, section_name)
     return section_config_data
+
+
+def copy_config(config_path, new_dir):
+    shutil.copy2(config_path, new_dir)
+
+
+def save_config(config_data, experiment_dir, experiment_no):
+    config_save_path = PathGenerator.get_config_pathname(experiment_dir, experiment_no)
+    with open(config_save_path, 'w') as configfile:
+        config_data.write(configfile)
+
+
+def save_config_with_custom_name(config_data, experiment_dir, custom_name):
+    config_save_path = PathGenerator.get_config_with_custom_name(experiment_dir, custom_name)
+    with open(config_save_path, 'w') as configfile:
+        config_data.write(configfile)
