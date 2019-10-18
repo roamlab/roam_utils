46da25fe9dd40ec2c90f5471debf5b65c01436e3
ehannigan <ejh2192@columbia.edu>
2019-10-18 13:21:01 -0400

    trajectory tests now pass, not much of a test, but at least the trajectories are able to be initialized

--- roam_utils/tests/test_trajectory/test_trajectory.py
+++ roam_utils/tests/test_trajectory/test_trajectory.py
@@ -15,29 +15,29 @@ class TestTrajectory(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         # runs once in class instantiation
-        # cls.temporary_experiment_dir = os.path.join(os.environ['ROBOT_LEARNING'],
-        #                                             'roam_learning/tests/temporary_experiments')
-        # if 'EXPERIMENTS_DIR' in os.environ:
-        #     cls.previous_experiment_dir = os.environ['EXPERIMENTS_DIR']
-        # else:
-        #     cls.previous_experiment_dir = None
-        # os.environ['EXPERIMENTS_DIR'] = cls.temporary_experiment_dir
+        cls.temporary_experiment_dir = os.path.join(os.environ['TEST_DIR'],
+                                                    'roam_learning/tests/temporary_experiments')
+        if 'EXPERIMENTS_DIR' in os.environ:
+            cls.previous_experiment_dir = os.environ['EXPERIMENTS_DIR']
+        else:
+            cls.previous_experiment_dir = None
+        os.environ['EXPERIMENTS_DIR'] = cls.temporary_experiment_dir
         cls.delta_t = .01
         cls.horizon = 15
         cls.X = np.arange(cls.horizon).reshape(-1, 1)
         cls.U = np.arange(cls.horizon-1).reshape(-1, 1)
         cls.time_array = cls.delta_t*np.arange(cls.horizon).reshape(-1, 1)
 
-    # @classmethod
-    # def tearDownClass(cls):
-    #     # runs once when class is torn down
-    #     for root, dirs, files in os.walk(cls.temporary_experiment_dir):
-    #         for f in files:
-    #             os.unlink(os.path.join(root, f))
-    #         for d in dirs:
-    #             shutil.rmtree(os.path.join(root, d))
-    #     if cls.previous_experiment_dir is not None:
-    #         os.environ['EXPERIMENTS_DIR'] = cls.previous_experiment_dir
+    @classmethod
+    def tearDownClass(cls):
+        # runs once when class is torn down
+        for root, dirs, files in os.walk(cls.temporary_experiment_dir):
+            for f in files:
+                os.unlink(os.path.join(root, f))
+            for d in dirs:
+                shutil.rmtree(os.path.join(root, d))
+        if cls.previous_experiment_dir is not None:
+            os.environ['EXPERIMENTS_DIR'] = cls.previous_experiment_dir
 
     def test_numpy_trajectory(self):
         X = np.arange(self.horizon).reshape(-1, 1)
@@ -48,8 +48,8 @@ class TestTrajectory(unittest.TestCase):
 
     def test_torch_trajectory(self):
         config_data = configparser.ConfigParser()
-        config_data.read()
-        device = TorchDevice(config_data, section_name)
+        config_data.read(os.path.join(os.environ['TEST_DIR'], 'test_trajectory/configs/torch_device.cfg'))
+        device = TorchDevice(config_data, 'torch_device')
         X = torch.tensor(self.X)
         U = torch.tensor(self.U)
         time_array = torch.tensor(self.delta_t*np.arange(self.horizon))
