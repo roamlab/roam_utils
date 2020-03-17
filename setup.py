5973f8573bad7d95272b264956125a8a25c52aee
Gagan Khandate <gagank@cs.columbia.edu>
2020-03-17 17:27:20 -0400

    Reduced dependencies
    Removed some packages that are not necessarily dependencies because
    we are only extending their functionality. A good example is PyTorch.
    We have TorchDevice and TorchOperations which only provide
    additionaly functionality. It is better to have torch installed by
    the project / library that uses roam_utils

--- setup.py
+++ setup.py
@@ -2,17 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
 name="roam_utils",
-version="0.1.0",
+version="1.1.0",
 author="ROAM Lab",
 description="ROAM Lab's package for utils",
 url="https://github.com/roamlab/roam_utils/",
 packages=[package for package in find_packages()
-                if package.startswith('roam_utils')],
+          if package.startswith('roam_utils')],
 install_requires = [
     'numpy',
-    'torch',
-    'matplotlib',
-    'configparser',
     'joblib',
    ]
 )
